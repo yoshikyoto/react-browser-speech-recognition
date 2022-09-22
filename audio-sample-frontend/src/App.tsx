f2ee32c4641579029c7e6585865b450b50759abf
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2022-09-22 12:53:40 +0900

    とりあえずそれなりに

--- audio-sample-frontend/src/App.tsx
+++ audio-sample-frontend/src/App.tsx
@@ -1,25 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 function App() {
+  // echoCancellation したものを useSpeechRecognition に渡すことができないらしい
   navigator.mediaDevices.getUserMedia({
     audio: {advanced: [{echoCancellation: true}]}
   })
   const {
     transcript,
+    finalTranscript,
     listening,
     resetTranscript,
     browserSupportsSpeechRecognition
   } = useSpeechRecognition();
-  console.log(listening)
+
+  const [document, setDocument] = useState<string>('');
+  const [finalizedPointer, setFinalizedPointer] = useState<number>(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (finalTranscript.length !== finalizedPointer) {
+        const updated = finalTranscript.slice(finalizedPointer)
+        setDocument(document + "\n\n" + updated)
+        setFinalizedPointer(finalTranscript.length)
+      }
+    }, 1000)
+  })
+
   return (
     <div className="App">
-      <button onClick={e => SpeechRecognition.startListening({ continuous: true })}>Start</button>
-      <button onClick={e => SpeechRecognition.stopListening()}>Stop</button>
-      <div>listening: {listening ? 'on' : 'off'}</div>
-      <div>ブラウザ対応: {browserSupportsSpeechRecognition ? 'yes' : 'no'}</div>
-      <p>{transcript}</p>
+      <h1>音声認識</h1>
+      <p>
+        Google Chrome のみ対応しています<br/>
+        {browserSupportsSpeechRecognition ?
+          'お使いのブラウザは音声認識に対応しています' :
+          'お使いのブラウザは音声認識に対応していません'}<br></br>
+      </p>
+      <p>
+        {listening ? '音声認識中' : 'スタートボタンを押してください'}
+      </p>
+      <button onClick={e => SpeechRecognition.startListening({ continuous: true })}>
+        音声認識スタート
+      </button>
+      <button onClick={e => SpeechRecognition.stopListening()}>
+        ストップ
+      </button>
+      <button onClick={e => {
+        setDocument('')
+        setFinalizedPointer(0)
+        resetTranscript()
+      }}>
+        リセット(全て削除)
+      </button>
+      <h2>現在認識中の文字</h2>
+      <p>{transcript.slice(-50)}</p>
+      <h2>エディタ</h2>
+      <p>音声認識が確定したものがテキストボックスに追加されていきます</p>
+      <div>
+        <textarea 
+          value={document} 
+          onChange={e => setDocument(e.target.value)}
+          style={{width:640, height:400}}>
+        
+        </textarea>
+      </div>
     </div>
   );
 }
